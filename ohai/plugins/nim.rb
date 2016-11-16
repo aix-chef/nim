5061945e97ec99a8a97c85a7ab33ef5be606b8fb
atos-hurstel <jerome.hurstel@atos.net>
2016-11-16 15:30:21 +0100

    add more threads

--- ohai/plugins/nim.rb
+++ ohai/plugins/nim.rb
@@ -35,7 +35,7 @@ Ohai.plugin(:NIM) do
   #         and all known lpp_sources
   #
   def parse_niminfo(niminfo_file = '/etc/niminfo')
-    niminfo = Hash.new
+    niminfo = {}
     niminfo_hash = File.open(niminfo_file) do |niminfo|
       niminfo_to_hash(niminfo)
     end
@@ -49,6 +49,124 @@ Ohai.plugin(:NIM) do
       niminfo['spots'] = spots
     end
     niminfo
+  end
+
+  class ::Hash
+    def deep_merge(second)
+        merger = proc { |key, v1, v2| Hash === v1 && Hash === v2 ? v1.merge(v2, &merger) : v2 }
+        self.merge(second, &merger)
+    end
+  end
+
+  # clients
+  #
+  # Determines if the niminfo configuration and oslevel for all the clients of a nim master
+  #
+  # Note: The oslevel command adds considerably to the execution time of the ohai plugin.
+  #
+  # == Parameters:
+  #   none
+  #
+  # == Returns:
+  #   Hash of Hashes: Hash with nim client name as key, niminfo hash as the value
+  #                   Also includes the oslevel
+  #
+  def clients
+    c_rsh = "/usr/lpp/bos.sysmgt/nim/methods/c_rsh"
+    threads = []
+	Thread.abort_on_exception = true
+    shell_out('/usr/sbin/lsnim -t standalone').stdout.each_line do |line|
+      client_name = line.split.first
+      begin
+        threads.push(Thread.new do
+	      ret = {}
+		  cmd_rc = shell_out("#{c_rsh} #{client_name} \"cat /etc/niminfo\"", timeout: 30).stdout
+          ret[client_name] = niminfo_to_hash(cmd_rc)
+          ret
+        end)
+        threads.push(Thread.new do
+	      ret = {}
+		  oslevel = shell_out("#{c_rsh} #{client_name} \"/usr/bin/oslevel -s\"", timeout: 30).stdout.chomp
+          ret[client_name] = { 'oslevel' => oslevel }
+          ret
+        end)
+        threads.push(Thread.new do
+	      ret = {}
+		  client_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{client_name}").stdout)
+          purge_superfluous_attributes(client_attributes)
+          ret[client_name] = { 'lsnim' => client_attributes }
+          ret
+        end)
+      rescue Ohai::Exceptions::Exec => e
+        if e.message.end_with? "returned 2" then
+          $stderr.puts "#{client_name} timed out"
+        else
+          $stderr.puts "#{client_name}: #{e.message}"
+        end
+      rescue Exception => e
+        $stderr.puts "#{client_name} exception: #{e.class.name}"
+        puts e.message
+      end
+    end
+    clients = {}
+    threads.each { |thr| clients = clients.deep_merge(thr.value)}
+    clients
+  end
+
+  # lpp_sources
+  #
+  # Identifies the lpp_sources available to a nim master
+  #
+  # == Parameters:
+  #   none
+  #
+  # == Returns:
+  #   Hash of Hashes: Hash with nim lpp_source resource name as key, with a hash of
+  #   attributes of each lpp_source as the value
+  #
+  def lpp_sources
+    threads = []
+    shell_out('/usr/sbin/lsnim -t lpp_source').stdout.each_line do |line|
+      threads.push(Thread.new do
+        ret = {}
+		lpp_source = line.split.first
+        lpp_source_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{lpp_source}").stdout)
+        purge_superfluous_attributes(lpp_source_attributes)
+        ret[lpp_source] = lpp_source_attributes
+		ret
+      end)
+    end
+    lpp_sources = {}
+    threads.each { |thr| lpp_sources.merge!(thr.value) }
+    lpp_sources
+  end
+
+  # spots
+  #
+  # Identifies the spots available to a nim master
+  #
+  # == Parameters:
+  #   none
+  #
+  # == Returns:
+  #   Hash of Hashes: Hash with nim spot name as key, with a hash of
+  #   attributes of each spot as the value
+  #
+  def spots
+    threads = []
+    shell_out('/usr/sbin/lsnim -t spot').stdout.each_line do |line|
+      threads.push(Thread.new do
+        ret = {}
+		spot = line.split.first
+        spot_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{spot}").stdout)
+        purge_superfluous_attributes(spot_attributes)
+        ret[spot] = spot_attributes
+		ret
+      end)
+    end
+    spots = {}
+    threads.each { |thr| spots.merge!(thr.value) }
+    spots
   end
 
   # niminfo_to_hash
@@ -67,29 +185,25 @@ Ohai.plugin(:NIM) do
   # == Returns:
   #   Hash: Key/Value pairs for each nim attribute defined in the niminfo file
   #
-  def niminfo_to_hash(niminfo_stream)
-    nim_hash = Hash.new
-
-    niminfo_stream.each_line do |line|
+  def niminfo_to_hash(string)
+    hash = {}
+    string.each_line do |line|
       line.chomp!
-
       # parse the key and value, each side of the '='
       if line =~ /^export\s+([[[:upper:]]_]+)=(.+)/ then
-        key, value = $1, $2
-
+        key, value = Regexp.last_match(1), Regexp.last_match(2)
         # normalise the key & remove quotes from value
         key.gsub!(/NIM_/, '')
         key.downcase!
         value.gsub!(/"/, '')
-
         # Hosts, routes and mounts are space-separated lists
         if key =~ /hosts|routes|mounts/ then
           value = value.split
         end
-        nim_hash[key] = value
+        hash[key] = value
       end
     end
-    nim_hash
+    hash
   end
 
   # is_master?
@@ -108,84 +222,6 @@ Ohai.plugin(:NIM) do
     nim['master']['configuration'] == 'master'
   end
 
-  # clients
-  #
-  # Determines if the niminfo configuration and oslevel for all the clients of a nim master
-  #
-  # Note: The oslevel command adds considerably to the execution time of the ohai plugin.
-  #
-  # == Parameters:
-  #   none
-  #
-  # == Returns:
-  #   Hash of Hashes: Hash with nim client name as key, niminfo hash as the value
-  #                   Also includes the oslevel
-  #
-  def clients
-    c_rsh = "/usr/lpp/bos.sysmgt/nim/methods/c_rsh"
-    clients = Hash.new
-    threads = []
-    client_list = shell_out('/usr/sbin/lsnim -t standalone').stdout
-    client_list.each_line do |line|
-      threads.push(Thread.new do
-        client_name = line.split.first
-        begin
-          cmd_rc = shell_out("#{c_rsh} #{client_name} \"cat /etc/niminfo\"", timeout: 30).stdout
-          client_niminfo = niminfo_to_hash(cmd_rc)
-          oslevel = shell_out("#{c_rsh} #{client_name} \"/usr/bin/oslevel -s\"", timeout: 30).stdout.chomp
-          client_niminfo['oslevel'] = oslevel
-          client_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{client_name}").stdout)
-          purge_superfluous_attributes(client_attributes)
-          client_niminfo['lsnim'] = client_attributes
-
-          clients[client_name] = client_niminfo
-
-        rescue Ohai::Exceptions::Exec => e
-          clients[client_name] = {}
-          if e.message.end_with? "returned 2" then
-            $stderr.puts "#{client_name} timed out"
-          else
-            $stderr.puts "#{client_name}: #{e.message}"
-          end
-
-        rescue Exception => e
-          clients[client_name] = {}
-          $stderr.puts "#{client_name} exception: #{e.class.name}"
-          puts e.message
-        end
-      end)
-    end
-    threads.each { |thr| thr.join }
-    clients=Hash[clients.sort]
-    clients
-  end
-
-  # lpp_sources
-  #
-  # Identifies the lpp_sources available to a nim master
-  #
-  # == Parameters:
-  #   none
-  #
-  # == Returns:
-  #   Hash of Hashes: Hash with nim lpp_source resource name as key, with a hash of
-  #   attributes of each lpp_source as the value
-  #
-  def lpp_sources
-    lpp_sources = Hash.new()
-    threads = []
-    shell_out('/usr/sbin/lsnim -t lpp_source').stdout.each_line do |line|
-      threads.push(Thread.new do
-        lpp_source = line.split.first
-        lpp_source_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{lpp_source}").stdout)
-        purge_superfluous_attributes(lpp_source_attributes)
-        lpp_sources[lpp_source] = lpp_source_attributes
-      end)
-    end
-    threads.each { |thr| thr.join }
-    lpp_sources
-  end
-
   # nim_attr_string_to_hash
   #
   # Parses a string of nim key/value attributes and returns the hash equivalent
@@ -196,49 +232,23 @@ Ohai.plugin(:NIM) do
   # == Returns:
   #   Hash:: nim attribute hash
   #
-  def nim_attr_string_to_hash(nim_atrr_string)
-    nim_attributes = Hash.new
-    nim_atrr_string.each_line do |line|
+  def nim_attr_string_to_hash(string)
+    hash = {}
+    string.each_line do |line|
       if line.start_with?(' ') then
         key, value = line.split(/=/)
-        nim_attributes[key.to_s.strip] = value.to_s.strip
+        hash[key.to_s.strip] = value.to_s.strip
       end
     end
-    nim_attributes
+    hash
   end
 
   # purge_superfluous_attributes
   #
-  def purge_superfluous_attributes(nim_hash)
+  def purge_superfluous_attributes(hash)
     %w(class type arch prev_state simages bos_license name).each do |attr|
-      nim_hash.delete attr
+      hash.delete attr
     end
-  end
-
-  # spots
-  #
-  # Identifies the spots available to a nim master
-  #
-  # == Parameters:
-  #   none
-  #
-  # == Returns:
-  #   Hash of Hashes: Hash with nim spot name as key, with a hash of
-  #   attributes of each spot as the value
-  #
-  def spots
-    spots = Hash.new()
-    threads = []
-    shell_out('/usr/sbin/lsnim -t spot').stdout.each_line do |line|
-      threads.push(Thread.new do
-        spot = line.split.first
-        spot_attributes = nim_attr_string_to_hash(shell_out("/usr/sbin/lsnim -l #{spot}").stdout)
-        purge_superfluous_attributes(spot_attributes)
-        spots[spot] = spot_attributes
-      end)
-    end
-    threads.each { |thr| thr.join }
-    spots
   end
 
   # collect_data
